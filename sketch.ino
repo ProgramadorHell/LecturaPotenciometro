51d52927baead9457fa7d878d0c0e151ed157476
ProgramadorHell <155093942+ProgramadorHell@users.noreply.github.com>
2024-01-01 20:41:24 +0100

    Add files via upload

--- sketch.ino
+++ sketch.ino
@@ -1,28 +1,12 @@
-//Señal pwm para servomotes
-const int pinPWM1 = 9; // Definir el pin PWM (pines 3, 5, 6, 9, 10 y 11 son PWM)
-const int pinPWM2 = 3;
-const int pinPWM3 = 5;
-int brillo = 0; // Variable para controlar el brillo del LED
+const int pinPotenciometro = A1; // Definir el pin del potenciómetro
+int valorPotenciometro = 0; // Variable para almacenar el valor leído
 
 void setup() {
-  pinMode(pinPWM1, OUTPUT); // Configurar el pin PWM como salida
+  Serial.begin(9600); // Inicializar la comunicación serie
 }
 
 void loop() {
-  for (brillo = 0; brillo <= 255; brillo += 5) { // Aumentar brillo gradualmente
-    analogWrite(pinPWM1, brillo); // Establecer el brillo del LED
-    //delay(50); // Esperar un breve tiempo
-    analogWrite(pinPWM2, brillo); // Establecer el brillo del LED
-    //delay(50); // Esperar un breve tiempo
-    analogWrite(pinPWM3, brillo); // Establecer el brillo del LED
-    delay(50); // Esperar un breve tiempo
-  }
-  for (brillo = 255; brillo >= 0; brillo -= 5) { // Disminuir brillo gradualmente
-    analogWrite(pinPWM1, brillo); // Establecer el brillo del LED
-   // delay(50); // Esperar un breve tiempo
-    analogWrite(pinPWM2, brillo); // Establecer el brillo del LED
-    //delay(50); // Esperar un breve tiempo
-    analogWrite(pinPWM3, brillo); // Establecer el brillo del LED
-    delay(50); // Esperar un breve tiempo
-  }
+  valorPotenciometro = analogRead(pinPotenciometro); // Leer valor del potenciómetro
+  Serial.println(valorPotenciometro); // Imprimir valor en el monitor serie
+  delay(500); // Esperar medio segundo
 }
